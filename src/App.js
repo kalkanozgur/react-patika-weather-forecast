dc957f151e26430c0c1324fd88af93bbd9367967
kalkanozgur <ahmetozgurkalkan@gmail.com>
2022-09-23 14:37:11 +0300

    weather forecast case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Select } from "antd";
+import { useCity } from "./context/CityContext";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { city, setCityByName, cityList } = useCity();
+
+	return (
+		<div className="App">
+			<header className="App-header">React Weather App</header>
+			<>
+				<Form />
+				<Weather city={city} />
+			</>
+		</div>
+	);
 }
 
 export default App;
